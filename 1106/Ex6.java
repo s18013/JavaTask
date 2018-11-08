6a8d08cc2050d2433e2477134d8d240b9da2f55d
s18013 <s18013@std.it-college.ac.jp>
2018-11-08 07:29:58 +0000

    En route

--- 1106/Ex6.java
+++ 1106/Ex6.java
@@ -1,99 +1,175 @@
+import  java.util.ArrayList;
+
 class Ex6{
 
-	public int [] ex6_1(){
-		
-		int [] ret = new int[1];
-		
-		return ret;
-	}
+    public int [] ex6_1(){
 
-	public double [] ex6_2(){
-		
-		double [] ret = new double[1];
-		
-		return ret;
-	}
+        int [] ret = new int[6];
+        for (int i = 5; i > 0; i--) {
+            ret[5-i] = i;
+        }
+        return ret;
+    }
 
-	public int [] ex6_3(int a, int b){
-		
-		int [] ret = new int[1];
-		
-		return ret;
-	}
+    public double [] ex6_2(){
 
-	public int [] ex6_4(int [] a){
-		
-		int [] ret = new int[1];
-		
-		return ret;
-	}
+        double [] ret ={1.1, 2.2, 3.3, 4.4, 5.5};
+        return ret;
+    }
 
-	public int ex6_5(int [] a, int key){
-		
-		int ret = 9999;
-		
-		return ret;
-	}
+    public int [] ex6_3(int a, int b){
 
-	public int ex6_6(int [] a, int key){
-		
-		int ret = 9999;
-		
-		return ret;
-	}
+        int [] ret = new int[a];
+        for (int i = 0; i < a; i++){
+            ret[i]  = b;
+        }
 
-	public void ex6_7(int [] a, int idx){
-		
-		return;
-	}
+        return ret;
+    }
 
-	public void ex6_8(int [] a, int idx, int n){
-		
-		return;
-	}
+    public int [] ex6_4(int [] a){
 
-	public void ex6_9(int [] a, int idx, int x){
-		
-		return;
-	}
+        int sum = 0;
+        int max = a[0];
+        int min = a[1];
 
-	public void ex6_10(int [] a, int [] b){
-		
-		return;
-	}
+        for(int i = 0; i < a.length; i++){
+           sum += a[i];
+           int v = a[i];
+           if (v > max) {
+               max = v;
+           }
+           if (v < min) {
+               min = v;
+           }
+        }
+        float ave = Math.round(sum / a.length);
 
-	public int [] ex6_11(int [] a){
-		
-		int [] ret = new int[0];
+        int [] ret = {sum, (int)ave, max, min};
+               return ret;
+    }
 
-		return ret;
-	}
+    public int ex6_5(int [] a, int key){
 
-	public int [] ex6_12(int [] a, int x){
-		
-		int [] ret = new int[0];
 
-		return ret;
-	}
+        for (int i = 0; i < a.length; i++){
+                if (key == a[i]){
+                    return i;
+                }
+            }
+        return -1;
 
-	public int [] ex6_13(int [] a, int idx){
-		
-		int [] ret = new int[0];
+    }
 
-		return ret;
-	}
-	
-	public int [] ex6_14(int [] a, int idx, int n){
-		
-		int [] ret = new int[0];
-		
-		return ret;
-	}
-	
-	public int [] ex6_15(int [] a, int idx, int x){
-		
-		int [] ret = new int[0];
-		
-		return ret;
-	}
+    public int ex6_6(int [] a, int key){
+
+        for (int i = a.length -1; i > 0; i--){
+            if (a[i] == key){
+                return i;
+            }
+        }
+        return -1;
+
+    }
+    public int [] ex6_7(int [] a, int idx){
+
+        int size = a.length;
+
+        if (idx > 0 && idx < a.length){
+            size--;
+        }
+
+        int [] ret = new int[size];
+
+        for (int src = 0, dst = 0; src < a.length; src++){
+            if (src == idx){
+                continue;
+            }
+            ret[dst++] = a[src];
+        }
+        return ret;
+
+    }
+
+    public int [] ex6_8(int [] a, int idx, int n){
+
+        int [] ret = new int[1];
+//        int size = a.length;
+//
+//        if (a.length >= idx + n){
+//            size -= n;
+//        }
+//        int [] ret = new int[size];
+//
+//        for (int src = 0, dst = 0; src < a.length;){
+//            if (src == idx){
+//                src += n;
+//            ret[dst++] = a[src++];
+//            }else {
+//            ret = a.clone();
+//            }
+//        }
+//
+        return ret;
+    }
+
+    public int [] ex6_9(int [] a, int idx, int x){
+
+      int [] ret;
+        if (idx >= 0 && idx < a.length ){
+            ret = new int[a.length + 1];
+            int src = 0;
+            int dst = 0;
+            while (src < a.length) {
+                if (src == idx){
+                    ret[dst++] = x;
+                }
+                ret[dst++] = a[src++];
+            }
+        }else {
+            ret = new int[a.length];
+            ret = a.clone();
+       }
+        return ret;
+    }
+
+    public void ex6_10(int [] a, int [] b){
+
+        return;
+
+    }
+
+    public int [] ex6_11(int [] a){
+
+        int [] ret = new int[0];
+        return ret;
+    }
+
+    public int [] ex6_12(int [] a, int x){
+
+        int [] ret = new int[0];
+
+        return ret;
+    }
+
+    public int [] ex6_13(int [] a, int idx){
+
+        int [] ret = new int[0];
+
+        return ret;
+    }
+
+    public int [] ex6_14(int [] a, int idx, int n){
+
+        int [] ret = new int[0];
+
+        return ret;
+    }
+
+    public int [] ex6_15(int [] a, int idx, int x){
+
+        int [] ret = new int[0];
+
+        return ret;
+    }
 }
